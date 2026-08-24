4a46e1f24f07b12a0e5a9f316e8f9815af431b7b
MagnunAVF <magnunavf@gmail.com>
2026-08-23 23:52:36 -0300

    feat: open and close an SDL window on quit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <SDL.h>
+#include <cstdio>
+
+namespace {
+constexpr int kWindowWidth = 800;
+constexpr int kWindowHeight = 600;
+}  // namespace
 
 int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        kWindowWidth, kWindowHeight, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
